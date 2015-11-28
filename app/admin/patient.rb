600c55923dacff6bc8dd6f3958814380182c27fe
Nazmul Hassan <sumon444@yahoo.com>
2015-11-28 20:05:06 +0100

    permitting patient_type_id in param

--- app/admin/patient.rb
+++ app/admin/patient.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Patient do
-  permit_params :name, :address, :national_id, :gender, :birthday, :status, :phone, :patient_type
+  permit_params :name, :address, :national_id, :gender, :birthday, :status, :phone, :patient_type_id
 
   index do
     selectable_column
