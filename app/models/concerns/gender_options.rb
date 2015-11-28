74f770e6a583063ff785a0db14562d191767c568
Raihan Mahmud Arman <raihan2006i@gmail.com>
2015-11-28 22:05:04 +0100

    Typo

--- app/models/concerns/gender_options.rb
+++ app/models/concerns/gender_options.rb
@@ -2,8 +2,6 @@ module GenderOptions
   extend ActiveSupport::Concern
 
   included do
-    self.inheritance_column = nil
-
     def self.gender_options
       (self::GENDERS || []).collect { |o| [o.humanize, o] }
     end
--- app/models/concerns/status_options.rb
+++ app/models/concerns/status_options.rb
@@ -2,8 +2,6 @@ module StatusOptions
   extend ActiveSupport::Concern
 
   included do
-    self.inheritance_column = nil
-
     def self.status_options
       (self::STATUSES || []).collect { |o| [o.humanize, o] }
     end
