afd9bdc712d027a981fbc32a8abc7dabbcafb01c
Nazmul Hassan <sumon444@yahoo.com>
2015-11-28 17:54:52 +0100

    implementing Patient model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151128131807) do
+ActiveRecord::Schema.define(version: 20151128155519) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,5 +53,19 @@ ActiveRecord::Schema.define(version: 20151128131807) do
 
   add_index "employees", ["email"], name: "index_employees_on_email", unique: true, using: :btree
   add_index "employees", ["reset_password_token"], name: "index_employees_on_reset_password_token", unique: true, using: :btree
+
+  create_table "patients", force: :cascade do |t|
+    t.string   "name"
+    t.string   "address"
+    t.string   "national_id"
+    t.string   "gender"
+    t.date     "birthday"
+    t.string   "status"
+    t.string   "phone"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "patients", ["national_id"], name: "index_patients_on_national_id", unique: true, using: :btree
 
 end
